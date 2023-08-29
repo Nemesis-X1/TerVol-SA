4e6b2a95b39cf01093c4eeb36b31999c6f71cf1a
marcelo <tronkito0230@gmail.com>
2023-08-28 20:40:39 -0400

    commit #1

---
+++ dashboard.php
@@ -0,0 +1,77 @@
+<?php //error_reporting(1); 
+?>
+<?php include('./constant/layout/head.php'); ?>
+<?php include('./constant/layout/header.php'); ?>
+
+<?php include('./constant/layout/sidebar.php'); ?>
+
+<?php
+
+
+$lowStockSql = "SELECT * FROM product WHERE status = 1";
+$lowStockQuery = $connect->query($lowStockSql);
+$countLowStock = $lowStockQuery->num_rows;
+
+$lowStockSql1 = "SELECT * FROM brands WHERE brand_status = 1";
+$lowStockQuery1 = $connect->query($lowStockSql1);
+$countLowStock1 = $lowStockQuery1->num_rows;
+
+$date = date('Y-m-d');
+$lowStockSql3 = "SELECT * FROM product WHERE  expdate<'" . $date . "' AND status = 1";
+//echo "SELECT * FROM product WHERE  expdate<='".$date."' AND status = 1" ;exit;
+$lowStockQuery3 = $connect->query($lowStockSql3);
+$countLowStock3 = $lowStockQuery3->num_rows;
+
+$lowStockSql2 = "SELECT * FROM orders WHERE delete_status =0";
+$lowStockQuery2 = $connect->query($lowStockSql2);
+$countLowStock2 = $lowStockQuery2->num_rows;
+
+//$connect->close();
+
+?>
+
+<style type="text/css">
+    .ui-datepicker-calendar {
+        display: none;
+    }
+</style>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<div class="page-wrapper">
+
+
+</div>
+</div>
+
+
+<?php include('./constant/layout/footer.php'); ?>
+<script>
+    $(function() {
+        $(".preloader").fadeOut();
+    })
+</script>
+<script>
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Contry', 'Mhl'], <?php echo $datavalue1; ?>
+        ]);
+
+        var options = {
+            title: 'World Wide Wine Production',
+            is3D: true
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('myChart'));
+        chart.draw(data, options);
+
+        var chart = new google.visualization.BarChart(document.getElementById('myChart1'));
+        chart.draw(data, options);
+    }
+</script>
+
+
+</div>
